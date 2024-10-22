bdf840a0cf8af6eb27d2723afb3b40dc304b96e9
George Mwangi <mwangigeorge648@gmail.com>
2024-10-22 21:23:12 +0300

    Threading Code

--- threading_demo.py
+++ threading_demo.py
@@ -17,18 +17,28 @@ def sleep_a_little(seconds):
 def main():
     calc_start_time = time.time()
 
+    current_threads = []
     for i in range(10):
-        value = (i + 1) * 100000000
+        value = (i + 1) * 100000
         t = threading.Thread(target=calc_sum_squares, args=(value,))
         t.start()
-        # current_thread.append(t)
+        current_threads.append(t)
+
+    for i in range(len(current_threads)):
+        current_threads[i].join()   # blocks execution until current thread is done
 
     print('Calculating sum of squares took', round(time.time() - calc_start_time, 1))
 
     sleep_start_time = time.time()
+
+    current_threads = []
     for i in range(1, 5):
         t = threading.Thread(target=sleep_a_little, args=(i,))
         t.start()
+        current_threads.append(t)
+
+    for i in range(len(current_threads)):
+        current_threads[i].join()
 
     print('Sleeping took', round(time.time() - sleep_start_time, 1))
 
